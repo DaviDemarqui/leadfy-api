0baaa951f137a44ab3c5df2170216a344a1f8f80
Davi Demarqui <davidemarqui@nextdigitalcompany.com>
2023-07-11 15:58:45 -0300

    VAI TOMA NO RABO O CARA QUE CRIOU ESSE TAL DE CORS

--- src/main/java/com/discern/api/configuration/CorsConfig.java
+++ src/main/java/com/discern/api/configuration/CorsConfig.java
@@ -1,26 +1,26 @@
 package com.discern.api.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+public class CorsConfig implements WebMvcConfigurer {
 
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
 
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 }
+
+
 
